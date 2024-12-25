6b578c5941ceb98bc29bb69c8a252447b0db84dd
PVG2514082 <118740743+PVG2514082@users.noreply.github.com>
2024-12-25 19:10:32 +0300

    Add files via upload

---
+++ sqll.py
@@ -0,0 +1,68 @@
+import sqlite3
+import json
+
+
+def add_user(type, password, name):
+    con = sqlite3.connect('data/instance/sportequip.db')
+    cur = con.cursor()
+    cur.execute("""INSERT INTO user (type, password, name) 
+                       VALUES (?, ?, ?)
+                    """, (type, password, name,))
+    con.commit()
+    return True
+
+
+def add_procurements(name, count, status):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""INSERT INTO procurements (name, count, status)
+                        VALUES (?, ?, ?)
+                    """, (name, count, status,))
+    con.commit()
+    return True
+
+
+def add_item(name, count, state):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""INSERT INTO inventory (name, count, state)
+                        VALUES (?, ?, ?)
+                    """, (name, count, state,))
+    con.commit()
+    return True
+
+
+def delete_item(name):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""DELETE FROM inventory WHERE name=?
+                                                    """, (name,))
+    con.commit()
+    return True
+
+
+def delete_user(name):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""DELETE FROM user WHERE name=?
+                                                    """, (name,))
+    con.commit()
+    return True
+
+
+def update_item(name, new_count):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""UPDATE inventory SET count=? WHERE name=?
+                                                            """, (new_count, name,))
+    con.commit()
+    return True
+
+
+def update_user(name, new_password):
+    con = sqlite3.connect("data/instance/sportequip.db")
+    cur = con.cursor()
+    cur.execute("""UPDATE user SET password=? WHERE name=?
+                                                        """, (new_password, name))
+    con.commit()
+    return True
